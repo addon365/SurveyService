1605ee147a6601c573de6f075c5a9fa660729ed4
Sathish Babu <sathishbabur@hotmail.com>
2018-07-04 18:23:19 +0530

    Workable Associates model implemented (But still investigation pending)

--- app/controllers/district.controller.js
+++ app/controllers/district.controller.js
@@ -4,17 +4,34 @@ const stateRepository = db.stateRepository;
 
 //Post a State
 exports.create = (req, res) => {
-    
-    var districtObj=districtRepository.build(req.body);
+    var tmpStateId = req.body.stateid;
+    if (tmpStateId == -1) {
+        res.status(400).send("State object not found");
+    }
+    stateRepository.findById(tmpStateId)
+        .then(stateObj => {
+            districtRepository.create(req.body)
+                .then(districtObj => {
+                    districtRepository.findById(districtObj.id,
+                        { include: { model: { State } } })
+                        .then(districtObjResult => {
+                            res.status(201).send(districtObjResult);
+                        });
 
-    districtRepository.create(req.body)
-        .then(response => {
-            res.send(response);
+                    // stateObj.addDistricts(districtObj)
+                    //     .then(() => {
+                    //         res.status(201).send(districtObj);
+                    //     });
+                });
+        })
+        .catch(message => {
+            res.status(500).send(message);
         });
 }
 //List a State
 exports.findAll = (req, res) => {
-    districtRepository.findAll().then(districts => {
-        res.status(200).send(districts);
-    });
+    districtRepository.findAll({ inclulde: { model: State } })
+        .then(districts => {
+            res.status(200).send(districts);
+        });
 };
